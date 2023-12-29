61410f577d48e5896562a9a38c70f3af3dfb20a9
Tickether <tickether@gmail.com>
2023-12-29 10:29:38 +0000

    part dark fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <div className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto max-[1024px]:border-b max-[1024px]:border-gray-300 max-[1024px]:bg-gradient-to-b max-[1024px]:from-zinc-200">
           <ConnectButton/>
         </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -33,7 +33,7 @@ export default function Home() {
             <Image
               src="/decent.png"
               alt="Decent Logo"
-              className="dark:invert"
+              className=""
               width={100}
               height={24}
               priority
@@ -42,7 +42,7 @@ export default function Home() {
             <Image
               src="/pooltogether.svg"
               alt="PoolTogether Logo"
-              className="dark:invert"
+              className=""
               width={100}
               height={24}
               priority
@@ -53,14 +53,14 @@ export default function Home() {
 
       <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative "
           src="/flipflop.png"
           alt="FlipFlop Logo"
           width={180}
           height={37}
           priority
         />
-        <p className="flex w-full justify-center text-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit static w-auto rounded-xl border bg-gray-200 p-4 dark:bg-zinc-800/30">
+        <p className="flex w-full justify-center text-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl static w-auto rounded-xl border bg-gray-200 p-4">
           Flip Flop into No Loss Prize Saving Pools🌊🏆&nbsp;
         </p>
       </div>
@@ -71,7 +71,7 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             <Image
-              className="inline-block relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              className="inline-block relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] "
               src="https://app.cabana.fi/icons/pUSDC.e.svg"
               alt=""
               width={30}
@@ -94,7 +94,7 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             <Image
-              className="inline-block relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              className="inline-block relative"
               src="https://app.cabana.fi/icons/pWETH.svg"
               alt=""
               width={30}
@@ -117,7 +117,7 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             <Image
-              className="inline-block relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              className="inline-block relative"
               src="https://app.cabana.fi/icons/pDAI.svg"
               alt=""
               width={30}
